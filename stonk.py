fd2c8d5adc5bb6358c92b5de708f8d2f4341a8fd
Sean Luong <luonghoaixuan@gmail.com>
2023-02-09 13:24:03 -0500

    Stop using Polygon and tell people to use /quote

--- stonk.py
+++ stonk.py
@@ -1,6 +1,5 @@
 from from_to import from_to
 from number_formatter import format_growth_rate, format_price
-import poly
 import yahoo
 
 HELP_CONTENT = """
@@ -10,25 +9,6 @@ HELP_CONTENT = """
   - $stonk $help: show this help message
   - $stonk AAPL: show the latest stock price of AAPL
 """
-
-# Users are used to the format XXX-USD for crypto (e.g. BTC-USD)
-# so we just check if the symbol contains a hyphen.
-def _is_crypto(symbol):
-  return '-' in symbol
-
-def use_polygon(client, symbol):
-  polygon_symbol = symbol
-  if _is_crypto(symbol):
-    parts = symbol.split('-')
-    crypto, currency = parts[0], parts[1]
-    polygon_symbol = "X:{}{}".format(crypto, currency)
-
-  try:
-    price, growth, name = poly.fetch_stock_price_data(client, polygon_symbol)
-  except Exception as error:
-    print("Failed to fetch data from Polygon {}".format(error))
-    return None, False
-  return (price, growth, name), True
 
 def use_yahoo(symbol):
   try:
@@ -51,13 +31,8 @@ async def handle_stonk(stockApiClient, symbol, message):
   if success:
     await reply(message, results, symbol)
     return
-
-  results, success = use_polygon(stockApiClient, symbol)
-  if success:
-    await reply(message, results, symbol)
-    return
   
-  await message.reply("Cannot get data for {}".format(symbol))
+  await message.reply("Cannot get data for {}. Try /quote to use Closing Bell".format(symbol))
 
 
 async def handle_help(message):
